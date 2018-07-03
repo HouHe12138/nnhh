43015e578165635f4502a1fb4109a011d374ac9b
houhe12138 <houheandxs@github.com>
2018-07-03 09:52:12 +0800

    add test2.py

---
+++ test2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/22 16:21
+# @Author  : nana
+from tkinter import *
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+import xlsxwriter
+
+root = Tk()
+root.geometry("500x150")
+
+
+class Reg(Frame):
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.lab1 = Label(frame, text="职位:")
+        self.lab1.grid(row=2, column=1, sticky=W)
+        self.ent1 = Entry(frame)
+        self.ent1.grid(row=2, column=3, sticky=W)
+        self.button = Button(frame, text="查询", command=self.Submit)
+        self.button.grid(row=4, column=3, sticky=E)
+
+    def Submit(self):
+
+        response = request.urlopen("https://search.51job.com/list/080200,000000,0000,00,9,99," + request.quote(self.ent1.get()) +
+                                   ",2,1.html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize"
+                                   "=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&lin"
+                                   "e=&specialarea=00&from=&welfare=")
+        res = response.read()
+        response.close()
+        # print(res[0, 100])
+
+        soup = BeautifulSoup(res, 'html.parser', from_encoding='gbk')
+        # print(soup)
+        # values = [input.attrs.get('value') for input in soup.select('div.el p.t1 input')]
+        pages = soup.select_one(".dw_page .td")
+
+        print(pages.get_text()[1:4])
+        urls = [k.get('href') for k in soup.select('div.el p.t1 a')]
+        key_num = {}
+        f = open('skill.txt', 'w',encoding='utf-8')
+        for url in urls:
+            position = request.urlopen(url)
+            pos = position.read()
+            position.close()
+            pos_need = BeautifulSoup(pos, 'html.parser', from_encoding='gbk')
+            # print(pos_need)
+            needs = pos_need.select(".tCompany_main .job_msg")
+            # need = soup.find_all('div',class_='bmsg job_msg inbox').find_all('p')
+            #uncn = re.compile(r'[\u0061-\u007a,\u0041-\u005a,\u3001,\u0020,\u002c,\u002e,\u002b,\u002d,\u0023,\u0028,\u0029,\u005c]')
+
+            uncn = re.compile('([a-zA-Z_\-\+# ]+)?', re.M)
+            num_re = re.compile('([a-zA-Z_\-\+# ]+)?', re.M)
+            blank_re = re.compile('\s+', re.M)
+            judge = ['!','(',')','/','.','+','-','*','#', '（', '）','a','b','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+            for need in needs:
+                uncn_txt = uncn.findall(need.get_text())
+
+                need_txt = []
+                for txt in uncn_txt:
+                    txt = txt.strip().lower()
+                    if txt != '' and (not txt in judge):
+                        if num_re.search(re.sub(blank_re, '', txt)):
+                            need_txt.append(txt)
+                            key_num[txt] = key_num.get(txt, 0) + 1
+                en = ",".join(need_txt)
+                f.write(en)
+                f.write('\n'+need.get_text().encode('utf-8').decode('utf-8')+'\n')
+                #print(en, '\n============================\n', need.get_text())
+                #print(en,'\n========\n')
+                #if len(need_txt) ==1:
+
+            # for need in needs:
+            #     en = "".join(uncn.findall(need.get_text()))
+            #     print(en)
+            #     # print(need.get_text())
+            # break
+            # print(values)
+            # print(urls)
+            # print(response.read())
+                #int(pages.get_text()[1:4])
+        for page in range(2,10):
+            response = request.urlopen("https://search.51job.com/list/080200,000000,0000,00,9,99," + request.quote(self.ent1.get()) +
+                                       ",2," + str(page) +
+                                       ".html?lang=c&stype=&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize"
+                                       "=99&providesalary=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&lin"
+                                       "e=&specialarea=00&from=&welfare=")
+            res = response.read()
+            response.close()
+
+            soup = BeautifulSoup(res, 'html.parser', from_encoding='gbk')
+            urls = [k.get('href') for k in soup.select('div.el p.t1 a')]
+            key_num = {}
+            for url in urls:
+                position = request.urlopen(url)
+                pos = position.read()
+                position.close()
+                pos_need = BeautifulSoup(pos, 'html.parser', from_encoding='gbk')
+                # print(pos_need)
+                needs = pos_need.select(".tCompany_main .job_msg")
+                # need = soup.find_all('div',class_='bmsg job_msg inbox').find_all('p')
+                # uncn = re.compile(r'[\u0061-\u007a,\u0041-\u005a,\u3001,\u0020,\u002c,\u002e,\u002b,\u002d,\u0023,\u0028,\u0029,\u005c]')
+
+                uncn = re.compile('([a-zA-Z/_\-\+# ]+)?', re.M)
+                num_re = re.compile('([a-zA-Z/_\-\+# ]+)?', re.M)
+                blank_re = re.compile('\s+', re.M)
+                judge = ['!', '(', ')', '/', '.', '+', '-', '*', '#', '（', '）','a','b','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+                for need in needs:
+                    uncn_txt = uncn.findall(need.get_text())
+                    need_txt = []
+                    for txt in uncn_txt:
+                        txt = txt.strip().lower()
+                        if txt != '' and (not txt in judge):
+                            if num_re.search(re.sub(blank_re, '', txt)):
+                                need_txt.append(txt)
+                                key_num[txt] = key_num.get(txt, 0) + 1
+                    en = ",".join(need_txt)
+                    f.write(en)
+                    f.write('\n' + need.get_text().encode('utf-8').decode('utf-8') + '\n')
+        print(key_num)
+        filename = '/Users/NN/Desktop/测试技能统计结果.xlsx'
+        test_need = xlsxwriter.Workbook(filename)
+        worksheet = test_need.add_worksheet('skill_needs')
+        headings = ['skills', 'count']
+        worksheet.write_row('A1', headings)
+        row = 1
+        col = 0
+        for key in key_num:
+            worksheet.write(row, col, key)
+            worksheet.write(row, col + 1, key_num[key])
+            row += 1
+        test_need.close()
+        f.close()
+
+root.title("搜索职位要求")
+app = Reg(root)
+root.mainloop()
